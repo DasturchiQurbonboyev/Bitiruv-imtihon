21b4fa38cd0833f02bb2e402c698dfc6c1295520
DasturchiQurbonboyev <dasturchiqurbonboyev@gmail.com>
2024-06-07 10:21:45 +0500

    cdscsdcs

--- src/components/hero-katalog/HeroKatalog.jsx
+++ src/components/hero-katalog/HeroKatalog.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
 import buttonimg from '../../assets/hero/buttonimg.png'
-import katalog1 from '../../assets/hero/katalog1.png'
+import katalog1 from '../../assets/katalog/katalog1.png'
+import katalog2 from '../../assets/katalog/katalog2.png'
+import katalog3 from '../../assets/katalog/katalog3.png'
+import katalog4 from '../../assets/katalog/katalog4.png'
+import katalog5 from '../../assets/katalog/katalog5.png'
+import katalog6 from '../../assets/katalog/katalog6.png'
 
 const HeroKatalog = () => {
+
+
+    let katalogs = [
+        {
+            img: katalog1,
+            title: "Люстры",
+            price: "От 540₽"
+        },
+        {
+            img: katalog2,
+            title: "Светильники",
+            price: "От 540₽"
+        },
+        {
+            img: katalog3,
+            title: "Бра",
+            price: "От 540₽"
+        },
+        {
+            img: katalog4,
+            title: "Торшеры",
+            price: "От 540₽"
+        },
+        {
+            img: katalog5,
+            title: "Настольные лампы",
+            price: "От 540₽"
+        },
+        {
+            img: katalog6,
+            title: "Споты",
+            price: "От 540₽"
+        },
+    ]
+
     return (
         <div className='pt-[102px] '>
             <div className='kontainer'>
@@ -14,22 +54,23 @@ const HeroKatalog = () => {
                     </div>
                 </div>
 
-                <div className='pt-10'>
-                    <div className='  px-4 py-[10px] gap-[70px]  max-w-[380px] rounded-[20px] justify-center  bg-[#F2F2F2]'>
-                        <div className='-mb-[25px] '>
-                            <p className='text-[20px]  font-[500] text-[#454545] '>Люстры</p>
-                        </div>
-                        <div className='text-end flex justify-end'>
-                            {/* <div> */}
-
-                            <img src={katalog1} alt="" />
-                            {/* </div> */}
-                        </div>
-                        <div className='flex -mt-[25px]  items-center gap-[11px]'>
-                            <p className='text-[14px]  font-[500] text-[#454545]'>От 540₽ </p>
-                            <img src={buttonimg} alt="button" />
-                        </div>
-                    </div>
+                <div className='pt-10 grid grid-cols-3 gap-5'>
+                    {
+                        katalogs?.map((el, inx) => (
+                            <div className='  px-4 py-[10px] gap-[70px]  max-w-[380px] rounded-[20px] justify-center  bg-[#F2F2F2]'>
+                                <div className='-mb-[25px] '>
+                                    <p className='text-[20px]  font-[500] text-[#454545] '>{el.title}</p>
+                                </div>
+                                <div className='text-end flex max-h-[200px] h-full justify-end'>
+                                    <img src={el.img} alt="" />
+                                </div>
+                                <div className='flex -mt-[25px]  items-center gap-[11px]'>
+                                    <p className='text-[14px]  font-[500] text-[#454545]'>{el.price}</p>
+                                    <img src={buttonimg} alt="button" />
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
