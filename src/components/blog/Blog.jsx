95e13b1976d05f75a69ecc7049e5d1da53b11fb0
DasturchiQurbonboyev <dasturchiqurbonboyev@gmail.com>
2024-06-07 10:42:53 +0500

    cdcsdcsd

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import buttonimg from '../../assets/hero/buttonimg.png'
 import blog1 from '../../assets/blog/blog1.png'
+import blog2 from '../../assets/blog/blog2.png'
+import blog3 from '../../assets/blog/blog3.png'
 import more_bnt from '../../assets/blog/more_bnt.png'
 
 const Blog = () => {
+
+
+
+    let blogs = [
+        {
+            title: "Как правильно освещать дом снаружи?",
+            data: "01.01.2024",
+            img: blog1
+        },
+        {
+            title: "Как правильно освещать дом снаружи?",
+            data: "01.01.2024",
+            img: blog2
+        },
+        {
+            title: "Как правильно освещать дом снаружи?",
+            data: "01.01.2024",
+            img: blog3
+        },
+        // {
+        //     title: "Как правильно освещать дом снаружи?",
+        //     data: "01.01.2024",
+        //     img: blog1
+        // },
+        // {
+        //     title: "Как правильно освещать дом снаружи?",
+        //     data: "01.01.2024",
+        //     img: blog2
+        // },
+        // {
+        //     title: "Как правильно освещать дом снаружи?",
+        //     data: "01.01.2024",
+        //     img: blog3
+        // },
+    ]
+
+
+
     return (
         <div className='kontainer'>
             <div className='pt-[50px]'>
@@ -16,19 +56,23 @@ const Blog = () => {
                         <img src={buttonimg} alt="button" />
                     </div>
                 </div>
-                <div>
-                    <div className='max-w-[420px]'>
-                        <div>
-                            <img src={blog1} alt="" />
-                        </div>
-                        <div className='flex  justify-between items-start   mt-[30px] mb-6'>
-                            <h5>Как правильно освещать дом снаружи?</h5>
-                            <img src={more_bnt} alt="" />
-                        </div>
-                        <div>
-                            <p>01.01.2024</p>
-                        </div>
-                    </div>
+                <div className='grid grid-cols-3 gap-5'>
+                    {
+                        blogs?.map((el, inx) => (
+                            <div key={inx} className='max-w-[420px]'>
+                                <div>
+                                    <img src={el.img} alt="" />
+                                </div>
+                                <div className='flex  justify-between items-start   mt-[30px] mb-6'>
+                                    <p className='font-[700] text-[20px] leading-[26px] text-[#454545]  '>{el.title}</p>
+                                    <img src={more_bnt} alt="" />
+                                </div>
+                                <div>
+                                    <p className='text-[14px] text-[#454545] font-[500]   '>{el.data}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
--- src/components/nornlightt/NornLight.jsx
+++ src/components/nornlightt/NornLight.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import buttonimg from '../../assets/hero/buttonimg.png'
-import night1 from '../../assets/hero/night1.png'
+import night1 from '../../assets/nornlight/night1.png'
+import night2 from '../../assets/nornlight/night2.png'
+import night3 from '../../assets/nornlight/night3.png'
+import night4 from '../../assets/nornlight/night4.png'
 
 const NornLight = () => {
+
+
+    let nights = [
+        {
+            img: night1,
+            description: "Бренды, проверенные временем и качеством",
+            title: "Только проверенные бренды"
+        },
+        {
+            img: night2,
+            description: "Ниже не будет нигде",
+            title: "Самые низкие цены"
+        },
+        {
+            img: night3,
+            description: "Доставляем по всей РФза 1-10 дней",
+            title: "Быстрая доствка"
+        },
+        {
+            img: night4,
+            description: "Более 1000 товаров",
+            title: "Большой ассортимент"
+        },
+    ]
+
+
+
+
     return (
         <div className='kontainer'>
             <div className='pt-[50px]'>
@@ -15,16 +46,20 @@ const NornLight = () => {
                         <img src={buttonimg} alt="button" />
                     </div>
                 </div>
-                <div>
-                    <div className='max-w-[310px] border  p-[40px] '>
-                        <div className='w-[80px] h-[80px] bg-[#454545] flex justify-center items-center rounded-md mb-[80px]'>
-                            <img src={night1} alt="" />
-                        </div>
-                        <div>
-                            <h5 className='text-[#454545] text-[20px] font-[700] leading-[26px]'>Только проверенные бренды</h5>
-                            <p className='text-[#454545] text-[16px] font-[400] leading-[20.8px] mt-[20px]'>Бренды, проверенные временем и качеством</p>
-                        </div>
-                    </div>
+                <div className='grid grid-cols-4 gap-5'>
+                    {
+                        nights?.map((el, inx) => (
+                            <div key={inx} className='max-w-[310px] border  p-[40px] '>
+                                <div className='w-[80px] h-[80px] bg-[#454545] flex justify-center items-center rounded-md mb-[80px]'>
+                                    <img src={el.img} alt="" />
+                                </div>
+                                <div>
+                                    <h5 className='h-[52px] text-[#454545] text-[20px] font-[700] leading-[26px]'>{el.title}</h5>
+                                    <p className='text-[#454545] text-[16px] font-[400] leading-[20.8px] mt-[20px]'>{el.description}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
