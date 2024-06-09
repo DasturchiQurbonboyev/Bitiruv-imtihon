8cd178cc4c6a361d0e7e806baa180db368cdf947
DasturchiQurbonboyev <dasturchiqurbonboyev@gmail.com>
2024-06-09 17:32:56 +0500

    cdsdcsdc

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,80 @@
-import React from 'react'
-import logo from '../../assets/navbar/logo.png'
-import katalog from '../../assets/navbar/katalog.png'
-import search from '../../assets/navbar/search.png'
-import like from '../../assets/navbar/like.png'
-import svyaz from '../../assets/navbar/svyaz.png'
-import cart from '../../assets/navbar/cart.png'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
-import { TfiAlignRight } from 'react-icons/tfi'
-import { FaRegHeart } from 'react-icons/fa'
-import { CgShoppingCart } from 'react-icons/cg'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { TfiAlignRight } from 'react-icons/tfi';
+import { FaRegHeart } from 'react-icons/fa';
+import { CgShoppingCart } from 'react-icons/cg';
+import { RiCloseLargeFill } from 'react-icons/ri';
+import { FiBarChart } from 'react-icons/fi';
+import logo from '../../assets/navbar/logo.png';
+import katalog from '../../assets/navbar/katalog.png';
+import search from '../../assets/navbar/search.png';
+import like from '../../assets/navbar/like.png';
+import svyaz from '../../assets/navbar/svyaz.png';
+import cart from '../../assets/navbar/cart.png';
+import './Navbar.css';
+
+const NavbarLinks = ({ onClick }) => (
+    <ul className='mt-5'>
+        <li className='py-[15px] text-center border-t-2'>
+            <Link to={"/about"} onClick={onClick}>О компании</Link>
+        </li>
+        <li className='py-[15px] text-center border-t-2'>
+            <Link to={"/shipping"} onClick={onClick}>Доставка и оплата</Link>
+        </li>
+        <li className='py-[15px] text-center border-t-2'>
+            <Link to={"/return"} onClick={onClick}>Возврат</Link>
+        </li>
+        <li className='py-[15px] text-center border-t-2'>
+            <Link to={"/garant"} onClick={onClick}>Гарантии</Link>
+        </li>
+        <li className='py-[15px] text-center border-t-2'>
+            <Link to={"/contact"} onClick={onClick}>Контакты</Link>
+        </li>
+        <li className='py-[15px] text-center border-t-2 border-b-2'>
+            <Link to={"/blog"} onClick={onClick}>Блог</Link>
+        </li>
+    </ul>
+);
+
+const ContactInfo = () => (
+    <div className='flex flex-col justify-center items-center gap-3 mt-8'>
+        <a href="+99888008904656">8 (800) 890-46-56</a>
+        <p>Заказать звонок</p>
+    </div>
+);
 
 const Navbar = () => {
+    const [navbarToggle, setNavbarToggle] = useState(false);
+
     return (
-        <header className=''>
+        <header>
             <div className='kontainer'>
-                <div className='navbar__top  '>
+                <div className={`bg-white pb-5 navbar__animation ${navbarToggle ? "navbar__open" : ""}`}>
+                    <div className='flex justify-between items-center py-[15px]'>
+                        <div>
+                            <RiCloseLargeFill onClick={() => setNavbarToggle(false)} className='cursor-pointer' size={28} />
+                        </div>
+                        <div className='flex gap-10'>
+                            <FaRegHeart className='cursor-pointer' size={25} />
+                            <FiBarChart className='cursor-pointer' size={25} />
+                            <CgShoppingCart className='cursor-pointer' size={27} />
+                        </div>
+                    </div>
+                    <NavbarLinks onClick={() => setNavbarToggle(false)} />
+                    <div className='bg-[#000] flex justify-center py-3 rounded-[100px] mt-[25px] gap-4 max-w-[330px] mx-auto'>
+                        <img className='h-[22px]' src={katalog} alt="katalog" />
+                        <p className='text-[#fff]'>Каталог</p>
+                    </div>
+                    <ContactInfo />
+                </div>
+                <div className='navbar__top'>
                     <ul className='flex justify-between items-center gap-[27px]'>
-                        <li><Link to={"/about"} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]' >О компании</Link></li>
-                        <li><Link to={"/shipping"} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]' >Доставка и оплата</Link></li>
-                        <li><Link to={"/return"} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]' >Возврат</Link></li>
-                        <li><Link to={"/garant"} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]' >Гарантии</Link></li>
-                        <li><Link to={"/contact"} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]' >Контакты</Link></li>
-                        <li><Link to={"/blog"} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]' >Блог</Link></li>
+                        <li><Link to={"/about"} onClick={() => setNavbarToggle(false)} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]'>О компании</Link></li>
+                        <li><Link to={"/shipping"} onClick={() => setNavbarToggle(false)} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]'>Доставка и оплата</Link></li>
+                        <li><Link to={"/return"} onClick={() => setNavbarToggle(false)} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]'>Возврат</Link></li>
+                        <li><Link to={"/garant"} onClick={() => setNavbarToggle(false)} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]'>Гарантии</Link></li>
+                        <li><Link to={"/contact"} onClick={() => setNavbarToggle(false)} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]'>Контакты</Link></li>
+                        <li><Link to={"/blog"} onClick={() => setNavbarToggle(false)} className='text-[#454545] text-[14px] font-[600] leading-[19.2px]'>Блог</Link></li>
                     </ul>
                     <div className='flex justify-center items-center gap-6'>
                         <a href="+99888008904656">8 (800) 890-46-56</a>
@@ -31,27 +83,26 @@ const Navbar = () => {
                 </div>
             </div>
             <nav className='py-[10px]'>
-                <div className='kontainer navbar  gap-[29px]'>
+                <div className='kontainer navbar gap-[29px]'>
                     <div className='flex items-center gap-5'>
-                        <div>
-                            <TfiAlignRight size={25} />
+                        <div className='navbar__open__btn'>
+                            <TfiAlignRight onClick={() => setNavbarToggle(true)} className='cursor-pointer' size={25} />
                         </div>
                         <Link to={"/"}>
                             <img src={logo} alt="logo" />
                         </Link>
                     </div>
                     <div className='navbar__res'>
-                        {/* <FaHeart /> */}
                         <FaRegHeart size={25} />
                         <CgShoppingCart size={27} />
                     </div>
-                    <div className='navbar__minu  justify-end flex-grow gap-[12px] items-end'>
-                        <div className=' navbar__katalog__btn '>
-                            <img className=' h-[22px]' src={katalog} alt="katalog" />
+                    <div className='navbar__minu justify-end flex-grow gap-[12px] items-end'>
+                        <div className='navbar__katalog__btn'>
+                            <img className='h-[22px]' src={katalog} alt="katalog" />
                             <p className='text-[#fff]'>Каталог</p>
                         </div>
                         <div className='navbar__search'>
-                            <input className=' outline-none w-full' type="text" name="" id="" placeholder='Поиск по товарам' />
+                            <input className='outline-none w-full' type="text" placeholder='Поиск по товарам' />
                             <img className='cursor-pointer' src={search} alt="search" />
                         </div>
                         <div className='flex items-end gap-[30px]'>
@@ -59,7 +110,7 @@ const Navbar = () => {
                                 <img src={like} alt="like" />
                                 <span>Избранное</span>
                             </div>
-                            <div className='svyaz  '>
+                            <div className='svyaz'>
                                 <img src={svyaz} alt="like" />
                                 <span>Сравнение</span>
                             </div>
@@ -71,8 +122,16 @@ const Navbar = () => {
                     </div>
                 </div>
             </nav>
+            <div>
+                <div className='navbar__res__search'>
+                    <input className='outline-none w-full' type="text" placeholder='Поиск по товарам' />
+                    <div>
+                        <img className='cursor-pointer' src={search} alt="search" />
+                    </div>
+                </div>
+            </div>
         </header>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
