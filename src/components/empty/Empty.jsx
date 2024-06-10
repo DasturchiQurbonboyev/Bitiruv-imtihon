3ff799eed5aa9ee3f01676e43bb141c073f25729
DasturchiQurbonboyev <dasturchiqurbonboyev@gmail.com>
2024-06-10 13:45:11 +0500

    csdcscssdcscwe

---
+++ src/components/empty/Empty.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Empty = () => {
+  return (
+    <div className=''>
+      <div className='kontainer flex justify-center'>
+        <img src="https://img.freepik.com/free-vector/hand-drawn-no-data-concept_52683-127818.jpg" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Empty
--- src/components/wishlist/Wishlist.jsx
+++ src/components/wishlist/Wishlist.jsx
@@ -5,6 +5,7 @@ import './Wishlist.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { FaHeart } from 'react-icons/fa'
 import { toggleWishlist } from '../../context/slice/wishlistSlice'
+import Empty from '../empty/Empty'
 
 
 const Wishlist = () => {
@@ -16,46 +17,50 @@ const Wishlist = () => {
     const dispatch = useDispatch()
 
 
+    if (wishes.length > 0) {
+        return (
+            <div className=''>
+                <div className='kontainer'>
+                    <div>
+                        <p className='wishlist__title'>Избранные товары</p>
+                    </div>
+                    <div className='products__list pt-14'>
+                        {
+                            wishes?.map((el) => (
+                                <div key={el.id} className='max-w-[320px] border px-[16px] py-[28px]'>
+                                    <div className='relative flex justify-center items-center mb-[32px]'>
+                                        <div className='absolute top-0 right-2'>
+                                            <FaHeart onClick={() => dispatch(toggleWishlist(el))} className='cursor-pointer size-6' />
+                                        </div>
+                                        <img className='product__img' src={product} alt="" />
 
-    return (
-        <div className=''>
-            <div className='kontainer'>
-                <div>
-                    <p className='wishlist__title'>Избранные товары</p>
-                </div>
-                <div className='products__list pt-14'>
-                    {
-                        wishes?.map((el) => (
-                            <div key={el.id} className='max-w-[320px] border px-[16px] py-[28px]'>
-                                <div className='relative flex justify-center items-center mb-[32px]'>
-                                    <div className='absolute top-0 right-2'>
-                                        <FaHeart onClick={() => dispatch(toggleWishlist(el))} className='cursor-pointer size-6' />
                                     </div>
-                                    <img className='product__img' src={product} alt="" />
-
-                                </div>
-                                <h5 className='mb-[20px] text-[#454545] text-[20px] leading-[22px] font-[500]    '>Встраиваемый светильник Novotech</h5>
-                                <div className='flex justify-between items-center'>
-                                    <div>
-                                        <p>
-                                            <del className='text-[#9F9F9F] text-[12px]'>
-                                                7 000₽
-                                            </del>
-                                        </p>
-                                        <p className='text-[#454545] text-[20px] leading-[22px] font-[700]'>
-                                            6 399₽
-                                        </p>
-                                    </div>
-                                    <div>
-                                        <img src={cart} alt="" />
+                                    <h5 className='mb-[20px] text-[#454545] text-[20px] leading-[22px] font-[500]    '>Встраиваемый светильник Novotech</h5>
+                                    <div className='flex justify-between items-center'>
+                                        <div>
+                                            <p>
+                                                <del className='text-[#9F9F9F] text-[12px]'>
+                                                    7 000₽
+                                                </del>
+                                            </p>
+                                            <p className='text-[#454545] text-[20px] leading-[22px] font-[700]'>
+                                                6 399₽
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <img src={cart} alt="" />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))
-                    }
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
+        )
+    }
+    return (
+        <Empty />
     )
 }
 
