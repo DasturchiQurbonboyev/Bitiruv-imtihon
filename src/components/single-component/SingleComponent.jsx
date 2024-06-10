2a2b88747ee5894b9e2f1945a56dd0a8fc37e780
DasturchiQurbonboyev <dasturchiqurbonboyev@gmail.com>
2024-06-10 08:38:17 +0500

    cddcsdcsdcsd

--- src/components/single-component/SingleComponent.jsx
+++ src/components/single-component/SingleComponent.jsx
@@ -17,7 +17,6 @@ const SingleComponent = () => {
 
     const singleData = useSelector(state => state.single.value)[0]
 
-    console.log(singleData);
     return (
         <div className='pt-[50px]'>
             <div className='kontainer'>
@@ -62,6 +61,59 @@ const SingleComponent = () => {
                                     <FaRegHeart size={25} />
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='pt-[40px]'>
+                    <p className='harakteristika__title '>Характеристика</p>
+                    <div className='harakteristika'>
+                        <div>
+                            <p>Цвет</p>
+                            <p>Жёлтый </p>
+                        </div>
+                        <div>
+                            <p>Год</p>
+                            <p>2016</p>
+                        </div>
+                        <div>
+                            <p>Диаметр колеса</p>
+                            <p>27.5</p>
+                        </div>
+                        <div>
+                            <p>Материал рамы</p>
+                            <p>Карбон</p>
+                        </div>
+                        <div>
+                            <p>Размер</p>
+                            <p>L</p>
+                        </div>
+                        <div>
+                            <p>Страна</p>
+                            <p>Швейцария</p>
+                        </div>
+                        <div>
+                            <p>Производитель</p>
+                            <p>Scott</p>
+                        </div>
+                        <div>
+                            <p>Покрышки</p>
+                            <p>Schwalbe Rocket Ron EVO / 2.1 127EPI Kevlar Bead Tubeless Easy / PaceStar compound</p>
+                        </div>
+                        <div>
+                            <p>Рама</p>
+                            <p>Scale Carbon / HMX-технология / технология IMP / Коническая  рулевая труба / BB92 / Технология SDS / Дропауты IDS SL</p>
+                        </div>
+                        <div>
+                            <p>Подседельный Штырь</p>
+                            <p>Ritchey WCS 700 Series: Carbon Link FlexLogic / 31.6mm 900 Series: Carbon 2B SDS / 34.9mm</p>
+                        </div>
+                        <div>
+                            <p>Седло</p>
+                            <p>Ritchey WCS Streem V3 Titanium rails</p>
+                        </div>
+                        <div>
+                            <p>Вилка</p>
+                            <p>Rock Shox SID RL3 Air / демпфер DNA3 3-режима / 15mm QR axle / коническая рулевая труба / Удалённая блокировка, регулировка отскока / ход 100mm</p>
                         </div>
                     </div>
                 </div>
